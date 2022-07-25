33f795b7d529d9609c116af7ba01419aa852eabc
Денис Харке <hda007@campus.mephi.ru>
2022-07-25 22:59:20 +0300

    add some features to window and add png icon

--- main.py
+++ main.py
@@ -2,7 +2,13 @@ import tkinter as tk
 
 window = tk.Tk()
 window.title("kek")
+h = 500
+w = 400
 
-window.geometry("500x400+500+300")
+
+window.geometry(f"{h}x{w}+500+300")
+window.resizable(True, True)
+window.minsize(300, 400)
+window.maxsize(800, 900)
 
 window.mainloop()
